c1831cdc5364174546d8c6f995b11c35c5afd6c0
khatokargauri <khatokargauri@gmail.com>
2026-02-11 22:27:52 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+print(city_name)
+pet_name = input("What's your pets name?\n")
+print(pet_name)
+print("Your band name could be " + city_name + " " + pet_name)
+
